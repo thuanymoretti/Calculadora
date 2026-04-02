02b1ce83f5dc755736f54e063c899839a9815eb4
thuanymoretti <thuany_moretti@estudante.sesisenai.org.br>
2026-04-02 08:44:39 -0300

    add - finalizei

--- calcu.js
+++ calcu.js
@@ -1,60 +1,22 @@
-function obterValores() {
-    let primeiroNumero = document.getElementById("primeiroNumero").value;
-    let segundoNumero = document.getElementById("segundoNumero").value;
-    if(primeiroNumero == "" || segundoNumero == "") {
-        mostrarResultado("Preencha todos os campos!");
-        return null;
-    }
+function Adicionar(valor) {
+    document.getElementById("visor").value += valor;
+}
 
-    primeiroNumero = parseFloat(primeiroNumero);
-    segundoNumero = parseFloat(segundoNumero);
-    if(isNaN(primeiroNumero) || isNaN(segundoNumero)) {
-        mostrarResultado("Digite apenas números válidos!");
-        return null;
-    }
+function Limpar() {
+    document.getElementById("visor").value = "";
+}
 
-    return { primeiroNumero, segundoNumero };
+function Apagar() {
+    let visor = document.getElementById("visor");
+    visor.value = visor.value.slice(0, -1);
 }
-function gerenciamentoDeCalculo(calculo) {
-    let resultadoOperacao = 0;
-    const valores = obterValores();
-    if(!valores) return;
 
-    switch(calculo) {
-        case 'somar':
-            resultadoOperacao = somar(valores);
-            break;
-        case 'subtrair':
-            resultadoOperacao = subtrair(valores);
-            break;
-        case 'multiplicar':
-            resultadoOperacao = multiplicar(valores);
-            break;
-        case 'dividir':
-            if(valores.segundoNumero == 0) {
-                mostrarResultado("Não é possível dividir por zero!");
-                return;
-            }
-            resultadoOperacao = dividir(valores);
-            break;
-        default: 
-            mostrarResultado("Não foi possivel realizar o calculo.");
-            return;
+function Calcular() {
+    let visor = document.getElementById("visor");
+
+    try {
+        visor.value = eval(visor.value);
+    } catch {
+        visor.value = "Erro";
     }
-    mostrarResultado("Resultado: " + resultadoOperacao);
-}
-function somar(valores) {
-    return valores.primeiroNumero + valores.segundoNumero;
-}
-function subtrair(valores) {
-    return valores.primeiroNumero - valores.segundoNumero;
-}
-function multiplicar(valores) {
-    return valores.primeiroNumero * valores.segundoNumero;
-}
-function dividir(valores) {
-    return valores.primeiroNumero / valores.segundoNumero;
-}
-function mostrarResultado(mensagem) {
-    document.getElementById("resultado").innerText = mensagem;
 }
